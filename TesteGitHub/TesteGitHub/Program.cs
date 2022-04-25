db223dc3f404d530d0e239f4a7b9ad5956235032
Guest 01 <guest01@alunos.sc.senac.br>
2022-04-25 20:09:53 -0300

    ajustes teste 2

---
+++ TesteGitHub/TesteGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+        }
+    }
+}
